064bead2cd8d029d69e80c3b9800ac8f12adb89b
Aletive <seo16hyun@gmail.com>
2020-05-24 23:09:13 +0900

    reload

---
+++ commands/botprofile.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require('discord.js')
+
+// 봇과 메세지 정보를 받고 이 명령어가 실행되었을때...
+module.exports = (bot, msg) => {
+  const { channel } = msg
+
+
+  channel.send(
+
+    new MessageEmbed()
+      .setColor(Math.floor(Math.random() * 0xffffff))
+      .addField('생일','2020-5-17일에 생성된 봇입니다')
+      .addField('닉네임','Dawn')
+      .addField('제작자','Qya,PMH')
+      .addField('명령어','h!도움말확인')
+      .addField('문의','𝓠𝔂𝓪.𝓟𝓔#0001')
+      .setThumbnail('https://cdn.discordapp.com/attachments/713393225632710708/714117219411296256/2.jpg')
+      .setFooter("제작자-𝓠𝔂𝓪.𝓟𝓔#5124,PMH")
+  )
+
+
+}
+module.exports.info = { name: '봇정보' }
--- commands/help.js
+++ commands/help.js
@@ -12,6 +12,7 @@ module.exports = (bot, msg) => {
       .addField('h!네코','neko사진이나옵니다.')
       .addField('h!정보','자신의 정보을 확인합니다.')
       .addField('h!날짜', '오늘의 날짜를 알려줍니다.')
+      .addField('h!봇정보','봇에 대한 정보를 불러옵니다.')
       .setColor(Math.floor(Math.random() * 0xffffff))
   )
 }
