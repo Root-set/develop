1c5a51cf5eb49b5a3c26478c4d1b493a92b069ef
Aletive <seo16hyun@gmail.com>
2020-05-29 16:16:47 +0900

    neko

--- commands/neko.js
+++ commands/neko.js
@@ -3,19 +3,12 @@ const { MessageEmbed } = require('discord.js')
 // 봇과 메세지 정보를 받고 이 명령어가 실행되었을때...
 module.exports = (bot, msg) => {
   const { channel } = msg
-
-
   channel.send(
-
     new MessageEmbed()
       .setColor(Math.floor(Math.random() * 0xffffff))
       .setImage("https://img.trinets.xyz/api/v1.0.0?target=neko&type=redirect")
-
   )
-
-
 }
 
-// 이 명령어의 이름은 ''
 module.exports.info = { name: '네코' }
 
