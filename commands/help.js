f227e30e4480faea6e3b492ce3d7de491592c4d8
Aletive <seo16hyun@gmail.com>
2020-05-30 22:40:47 +0900

    help

--- commands/help.js
+++ commands/help.js
@@ -5,10 +5,11 @@ module.exports = (bot, msg) => {
 
 
   channel.send(
+
     new MessageEmbed()
-      .addField('h!도움말','봇의 명령어를 확인할수있습니다.')
-      .addField('h!핑','봇의핑을 확인할수있습니다.')
-      .addField('h!네코','neko사진이나옵니다.')
+      .addField('h!도움말','봇의 명령어를 확인할 수 있습니다.')
+      .addField('h!핑','봇의 핑을 확인할 수 있습니다.')
+      .addField('h!네코','네코 히히힇.')
       .addField('h!정보','자신의 정보을 확인합니다.')
       .addField('h!날짜', '오늘의 날짜를 알려줍니다.')
       .addField('h!봇정보','봇에 대한 정보를 불러옵니다.')
