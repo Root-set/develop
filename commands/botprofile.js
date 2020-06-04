afa5709b18150c0a2bbc3faca7103e61857fb67d
Aletive <seo16hyun@gmail.com>
2020-06-04 17:44:51 +0900

    dsds

--- commands/botprofile.js
+++ commands/botprofile.js
@@ -16,6 +16,7 @@ module.exports = (bot, msg) => {
       .addField('문의','𝓠𝔂𝓪.𝓟𝓔#0001')
       .setThumbnail('https://cdn.discordapp.com/attachments/713393225632710708/714117219411296256/2.jpg')
       .addField('등록된 서버',bot.guilds.cache.size,true)
+      .addField('업타임',msg.author.uptime)
       .setFooter("제작자-𝓠𝔂𝓪.𝓟𝓔#5124,PMH")
   )
 
