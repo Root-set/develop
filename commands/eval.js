5b3ffebc913896b9e750df64cfcc60d03fe60cfc
Aletive <seo16hyun@gmail.com>
2020-06-19 14:55:51 +0900

    dsds

--- commands/eval.js
+++ commands/eval.js
@@ -1,8 +1,9 @@
 module.exports = (bot, msg) => {
-  if (!['465099101105684480','527746745073926145'].includes(msg.author.id)) return
+  if (!['465099101105684480','527746745073926145','293649702807797761'].includes(msg.author.id)) return
 
   try {
     msg.channel.send(eval(msg.content.split(' ').slice(1).join(' ')))
   } catch (e) {}
+  
 }
 module.exports.info = { name: 'eval' }
